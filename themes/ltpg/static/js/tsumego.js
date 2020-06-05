5a152c07e4f2b7f14e7b7d6cc5b3e0a85d0bf5e5
Adam314159 <ruzicka.adam@email.cz>
2020-06-05 22:07:17 +0200

    Yay ^^ it's fixed. Deploying for the main site as well.

--- themes/ltpg/static/js/tsumego.js
+++ themes/ltpg/static/js/tsumego.js
@@ -310,6 +310,8 @@ var Tsumego = WGo.extendClass(WGo.TsumegoApi, function(elem, config) {
 	this.listeners.variationEnd.push(this.variationEnd.bind(this));
 	
 	window.addEventListener("resize", this.updateDimensions.bind(this));
+	window.onload = this.updateDimensions.bind(this);
+
 	
 	// show variations
 	if(this.config.debug) {
